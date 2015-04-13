bccff2082d0973c90235dc650d4c0fe4733a14c5
js02sixty <js02sixty@gmail.com>
2015-04-12 22:19:20 -0400

    fresh blueprint for angular client

---
+++ candlemaker/client/__init__.py
@@ -0,0 +1,17 @@
+'''
+Created on Apr 12, 2015
+
+@author: js02sixty
+'''
+
+from flask import Blueprint
+
+client = Blueprint('home', __name__, static_folder='static', static_url_path='')
+
+@client.route('/')
+@client.route('/admin')
+@client.route('/products')
+@client.route('/about')
+@client.route('/contact')
+def index():
+    return client.send_static_file('index.html')
